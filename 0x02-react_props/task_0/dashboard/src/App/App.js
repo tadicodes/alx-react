5bfff59159b156fc78e8488c14512f88f76e1602
tadicodes <tadishongedza@gmail.com>
2024-05-14 06:51:11 -0700

    commit

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer'
+
+function App() {
+  return (
+    <>
+    <Notifications />
+    <div className="App">
+      <Header />
+      <Login />
+      <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
